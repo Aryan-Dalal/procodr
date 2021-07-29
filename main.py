fb1c461b73c6528e04539dae9d1c293dc4074958
falkenmaze <79623930+falkenmaze@users.noreply.github.com>
2021-07-29 20:39:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #put n space seperated integers into array
 #arr = list(map(int, input().split()))
+
 #put letters or digits of a word or number into array
 #WORD
 #list(word)
 #Number
 #arr = [int(x) for x in str(number)]
+
+# taking input on the same line
+#n,m = [int(x) for x in input().split()]
 
 t = int(input())
 i = 0
