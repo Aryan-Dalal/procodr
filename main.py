432f12047c5a496854a5bd144368b55b7f8b8854
falkenmaze <79623930+falkenmaze@users.noreply.github.com>
2021-07-30 19:34:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@
 # taking input on the same line
 #n,m = [int(x) for x in input().split()]
 
-# #PROBLEM
+#PROBLEM
 # Now that Chef has finished baking and frosting his cupcakes, it's time to package them. 
 # Chef has N cupcakes, and needs to decide how many cupcakes to place in each package. 
 # Each package must contain the same number of cupcakes. 
@@ -18,7 +18,12 @@
 # Chef makes as many packages as possible. Chef then gets to eat the remaining cupcakes. Chef enjoys eating cupcakes very much. 
 # Help Chef choose the package size A that will let him eat as many cupcakes as possible.
 
+# Input
+# Input begins with an integer T, the number of test cases. Each test case consists of a single integer N, the number of cupcakes.
 
+# Output
+# For each test case, output the package size that will maximize the number of leftover cupcakes. 
+# If multiple package sizes will result in the same number of leftover cupcakes, print the largest such size.
 
 t = int(input())
 i = 0
