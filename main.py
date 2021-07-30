b2bcc63c36a127dc82114aee2aaac958411bc18a
falkenmaze <79623930+falkenmaze@users.noreply.github.com>
2021-07-30 19:33:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,16 @@
 # taking input on the same line
 #n,m = [int(x) for x in input().split()]
 
+# #PROBLEM
+# Now that Chef has finished baking and frosting his cupcakes, it's time to package them. 
+# Chef has N cupcakes, and needs to decide how many cupcakes to place in each package. 
+# Each package must contain the same number of cupcakes. 
+# Chef will choose an integer A between 1 and N, inclusive, and place exactly A cupcakes into each package. 
+# Chef makes as many packages as possible. Chef then gets to eat the remaining cupcakes. Chef enjoys eating cupcakes very much. 
+# Help Chef choose the package size A that will let him eat as many cupcakes as possible.
+
+
+
 t = int(input())
 i = 0
 while i<t:
